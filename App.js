d30a87926d2f3fe48a8c4229dc342646863c1727
Facundo <cailloureloaded@outlook.com>
2022-03-23 19:34:35 -0300

    Se crea la app

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>¡Hola, Coder!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
