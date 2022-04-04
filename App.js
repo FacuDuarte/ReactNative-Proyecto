e02136e7196a8a3de66ef3134be5a54e3f218ff2
Facundo <cailloureloaded@outlook.com>
2022-04-04 19:04:12 -0300

    Commit segunda actividad

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  Text, 
+  View, 
+  FlatList, 
+} from 'react-native';
+import ModalItem  from './components/Modal';
+import AddItem from './components/AddItem';
+import './style';
 
-import { StatusBar } from 'expo-status-bar';
+export default function App() {  
+  const [ counter, setCounter ] = useState(3);
+  const [ listItem, setListItem ] = useState([{id:1, value:'Juan'},{id:2, value:'Pedro'}]);
+  const [ itemSelected, setItemSelected ] = useState({});
+  const [ modalVisible, setModalVisible ] = useState(false);
 
-export default function App() {
+  
+  const onHandlerDelete = id => { 
+      console.log("Item " + itemSelected.value + " Eliminado");
+      setListItem( currenItems => currenItems.filter( item => item.id !== id ));
+      setItemSelected({});
+      setModalVisible(!modalVisible);
+  }
+  const onHandlerModal = id => {
+    setItemSelected(listItem.filter( item => item.id === id)[0]);
+    setModalVisible(!modalVisible);
+  }  
+  const closeModal = () => {
+    setModalVisible(!modalVisible);
+  }
+
+  const agregarItem = (textItem) => {   
+    console.log("Se Agrega.");
+    if(textItem!=="") {
+      console.log("Se agrego el item: " + textItem);
+      setListItem( currenItems => 
+        [...currenItems, {id: counter, value: textItem}]        
+      )
+      setCounter(counter + 1);
+    }
+  }
+  
+  const renderItem = data => 
+      <Text 
+        style={styles.listItem} 
+        onPress={onHandlerModal.bind(this, data.item.id)}>
+          * {data.item.value} ({data.item.id})
+      </Text>
+
   return (
     <View style={styles.container}>
-      <Text>¡Hola, Coder!</Text>
-      <StatusBar style="auto" />
+      <AddItem onAddItem={agregarItem}/>      
+      <View style={styles.listItemContainer}>
+        <FlatList
+          data={listItem}
+          renderItem={renderItem}
+          keyExtractor={ item => item.id }
+        />
+      </View>
+      <ModalItem onDelete={onHandlerDelete} item={itemSelected} visible={modalVisible} onCancel={closeModal}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
